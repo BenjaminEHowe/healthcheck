BenjaminEHowe/healthcheck

commit e3437076fb4c79da03562e2dc09a08ac08a15b81

Fixes issue #12

Spec.pm
@@ -32,13 +32,19 @@ sub cpu {
 
 sub memory {
     my $mem = `cat /proc/meminfo`;
+    my $unit;
     if ($mem =~ m/MemTotal:\s*(\d+)\s*kB/) {
-        $mem = sprintf "%.0f", $1 / (1024*1024);
+        $mem = sprintf "%.0f", $1 / (1024);
+        $unit = 'MB';
+        if ($mem > 1024) {
+            $mem /= 1024;
+            $unit = 'GB';
+        }
     } else {
         warn "Couldn't work out how much memory this system has from /proc/meminfo :(\n";
         exit(1);
     }
-    return "${mem}GB";
+    return "${mem}${unit}";
 }
 
 sub disks {

commit 5a3427a6d8794231995616c843e21263cd4fc9db

Fixed a bug introduced in commit e3437076fb4c79da03562e2dc09a08ac08a15b81

Spec.pm
@@ -34,12 +34,13 @@ sub memory {
     my $mem = `cat /proc/meminfo`;
     my $unit;
     if ($mem =~ m/MemTotal:\s*(\d+)\s*kB/) {
-        $mem = sprintf "%.0f", $1 / (1024);
+        $mem = $1 / 1024;
         $unit = 'MB';
         if ($mem > 1024) {
             $mem /= 1024;
             $unit = 'GB';
         }
+        $mem = sprintf "%.0f", $mem;
     } else {
         warn "Couldn't work out how much memory this system has from /proc/meminfo :(\n";
         exit(1);